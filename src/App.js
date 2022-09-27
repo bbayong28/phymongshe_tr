5470eb75fba3594debb48077fb4fdafd8c865f2a
bbayong28 <ghkdud5795@hanmail.net>
2022-09-27 16:21:03 +0900

    20220927

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './pages/Main';
+import Footer from './components/Footer';
+import './css/reset.css'
+//https://desipossa.github.io/shop_cra/assets/data.json
+
+const App = () => {
+  return (
+      <>
+          <Header />
+          <Main />
+          <Footer />
+      </>
+  )
+}
+
+export default App
